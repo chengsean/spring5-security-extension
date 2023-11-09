d7d0142a588b105a1397471ff90a2e4fcf619998
chengsean <chengsz7890@gmail.com>
2023-11-09 10:56:48 +0800

    代码提交

---
+++ src/main/java/org/gengshao/spring5securityextension/Spring5SecurityExtensionApplication.java
@@ -0,0 +1,13 @@
+package org.gengshao.spring5securityextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5SecurityExtensionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5SecurityExtensionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gengshao/spring5securityextension/Spring5SecurityExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gengshao.spring5securityextension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5SecurityExtensionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
